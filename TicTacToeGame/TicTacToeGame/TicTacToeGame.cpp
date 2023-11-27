f169d21cf65d63951db70a063087492b0201df34
Reece <reecebellchambers98@gmail.com>
2023-11-27 23:51:09 +0000

    Game finished possible testing.

--- TicTacToeGame/TicTacToeGame/TicTacToeGame.cpp
+++ TicTacToeGame/TicTacToeGame/TicTacToeGame.cpp
@@ -8,14 +8,36 @@ void playersTurn();
 void updateGrid(bool isPlayer1, bool isPlayer2, char playersChoice);
 void player1Choice(bool player1, char choice);
 void player2Choice(bool player1, char choice);
-//char tryAgain(std::vector<char> gridNum, char choice);
 bool winCondition(std::vector<char> gridNum);
+void introduction();
 
 int main()
 {
+    introduction();
     playersTurn();
-
     return 0;
+}
+
+void introduction()
+{
+    std::cout << "Welcome to TicTacToe Player vs Player Edition!\n";
+    std::cout << "To win the game the player must match 3 in a row either vertically, horizontally, or diagonally.\n";
+    std::cout << "On your turn, input the number that matches the place on the grid.\n";
+    std::cout << "Player 1 will have 'X's as their counter.\n";
+    std::cout << "Player 2 will have 'O's as their counter.\n";
+    std::cout << "WARNING!!! If you place a counter that already has a counter placed there you will be punished by ";
+    std::cout << "losing your turn so becareful when placing a counter!\n";
+    std::cout << "Please enjoy the game!\n";
+
+    std::cout << "   |   |   \n";
+    std::cout << " " << gridNum[0] << " | " << gridNum[1] << " | " << gridNum[2] << " \n";
+    std::cout << "___|___|___\n";
+    std::cout << "   |   |   \n";
+    std::cout << " " << gridNum[3] << " | " << gridNum[4] << " | " << gridNum[5] << " \n";
+    std::cout << "___|___|___\n";
+    std::cout << "   |   |   \n";
+    std::cout << " " << gridNum[6] << " | " << gridNum[7] << " | " << gridNum[8] << " \n";
+    std::cout << "   |   |   \n";
 }
 
 void playersTurn()
@@ -33,7 +55,6 @@ void playersTurn()
         // Turn change conditions between player 1 & 2
         if (isPlayer1)
         {
-            //updateGrid();
             std::cout << "Turn: Player 1\n";
             std::cout << "Please pick a space on the board that is free:";
             std::cin >> playersChoice;
@@ -43,7 +64,6 @@ void playersTurn()
         }
         else if (isPlayer2)
         {
-            //updateGrid();
             std::cout << "Turn: Player 2\n";
             std::cout << "Please pick a space on the board that is free:";
             std::cin >> playersChoice;
@@ -59,13 +79,14 @@ void playersTurn()
         // Turn amount will increase after every turn taken to end the game.
         turnAmount++;
 
-        // If the turn amount has reach the max turns end the game.
+        // If the turn amount has reach the max turns, it will be a draw.
         if (turnAmount > 8)
         {
             gameOver = true;
             std::cout << "It is a draw!\n";
         }
 
+        // A player has won the game.
         if (winCondition(gridNum))
         {
             gameOver = true;
@@ -95,16 +116,6 @@ void updateGrid(bool isPlayer1, bool isPlayer2, char playersChoice)
     std::cout << " " << gridNum[6] << " | " << gridNum[7] << " | " << gridNum[8] << " \n";
     std::cout << "   |   |   \n";
 }
-
-//char tryAgain(std::vector<char> gridNum, char choice)
-//{
-//    while ((gridNum[0] == 'X' || gridNum[0] == 'O') && (choice == 49))
-//    {
-//        std::cout << "Please choose another option!\n";
-//        std::cin >> choice;
-//        return choice;
-//    }
-//}
 
 bool winCondition(std::vector<char> gridNum)
 {
@@ -199,131 +210,113 @@ void player1Choice(bool player1, char choice)
 {
     if ((player1) && (choice == 49) && (gridNum[0] == 'X'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player1) && (choice == 49) && (gridNum[0] == 'O'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player1) && (choice == 50) && (gridNum[1] == 'X'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player1) && (choice == 50) && (gridNum[1] == 'O'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player1) && (choice == 51) && (gridNum[2] == 'X'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player1) && (choice == 51) && (gridNum[2] == 'O'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player1) && (choice == 52) && (gridNum[3] == 'X'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player1) && (choice == 52) && (gridNum[3] == 'O'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player1) && (choice == 53) && (gridNum[4] == 'X'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player1) && (choice == 53) && (gridNum[4] == 'O'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player1) && (choice == 54) && (gridNum[5] == 'X'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player1) && (choice == 54) && (gridNum[5] == 'O'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player1) && (choice == 55) && (gridNum[6] == 'X'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player1) && (choice == 55) && (gridNum[6] == 'O'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player1) && (choice == 56) && (gridNum[7] == 'X'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player1) && (choice == 56) && (gridNum[7] == 'O'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player1) && (choice == 57) && (gridNum[8] == 'X'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player1) && (choice == 57) && (gridNum[8] == 'O'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
-    //
+    // A 'X' will be placed on the grid
 
     if (player1 && choice == 49)
     {
@@ -371,133 +364,113 @@ void player2Choice(bool player2, char choice)
 {
     if ((player2) && (choice == 49) && (gridNum[0] == 'X'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player2) && (choice == 49) && (gridNum[0] == 'O'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player2) && (choice == 50) && (gridNum[1] == 'X'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player2) && (choice == 50) && (gridNum[1] == 'O'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player2) && (choice == 51) && (gridNum[2] == 'X'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player2) && (choice == 51) && (gridNum[2] == 'O'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player2) && (choice == 52) && (gridNum[3] == 'X'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player2) && (choice == 52) && (gridNum[3] == 'O'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player2) && (choice == 53) && (gridNum[4] == 'X'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player2) && (choice == 53) && (gridNum[4] == 'O'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player2) && (choice == 54) && (gridNum[5] == 'X'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player2) && (choice == 54) && (gridNum[5] == 'O'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player2) && (choice == 55) && (gridNum[6] == 'X'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player2) && (choice == 55) && (gridNum[6] == 'O'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player2) && (choice == 56) && (gridNum[7] == 'X'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player2) && (choice == 56) && (gridNum[7] == 'O'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player2) && (choice == 57) && (gridNum[8] == 'X'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
     if ((player2) && (choice == 57) && (gridNum[8] == 'O'))
     {
-        //tryAgain(gridNum, choice);
         std::cout << "You chose a space unavailable, now you lose your turn!\n";
         return;
     }
 
-    //
-
-    // Player2
+    // An 'O' will be placed on the grid
     if (player2 && choice == 49)
     {
         gridNum[0] = 'O';
